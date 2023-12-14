054de119f4b4cae52b5b33080f5df59b5bdd3c35
danielng <dacnguyen9101@gmail.com>
2023-12-15 01:01:07 +0700

    chore: add gitsha healthcheck

--- src/controllers/HealthCheckController.ts
+++ src/controllers/HealthCheckController.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+
+import {
+	AppConfigurationService,
+	Types as TConfig,
+} from '../modules/core/configuration';
 
 
 @Controller()
 export class HealthCheckController {
+	constructor(
+		@Inject(TConfig.CFG_SVC) private readonly _cfgSvc: AppConfigurationService
+	) {}
+
 	@Get('/health')
 	public getHealth() {
 		return { status: 'Ok' };
@@ -11,7 +20,7 @@ export class HealthCheckController {
 	@Get('/env')
 	public getEnv() {
 		return {
-			GIT_SHA: process.env.GIT_SHA,
+			GIT_SHA: this._cfgSvc.gitSha,
 			NODE_ENV: process.env.NODE_ENV,
 		};
 	}
